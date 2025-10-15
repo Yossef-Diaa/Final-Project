e532146eef528cb1babdd3ce164ad831e721401e
Yossef-Diaa <yossefdiaa79@gmail.com>
2025-10-15 21:45:01 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import authRoutes from './routes/authRoutes.js'; 
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
